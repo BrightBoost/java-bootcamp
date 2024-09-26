fabe95859697be0bef0d85ab9c98564d13b6d682
maaike <maaike@maaike.nl>
2024-09-26 22:59:28 +0200

    add

---
+++ src/main/java/com/pluralsight/Calculator.java
@@ -0,0 +1,28 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // grab the operation
+//        System.out.println("Do you want to add or subtract?");
+//        String operation = scanner.nextLine();
+
+        // grab the first number
+        System.out.println("Enter nr 1:");
+        float num1 = scanner.nextFloat();
+
+        // grab the second number
+        System.out.println("Enter nr 2:");
+        float num2 = scanner.nextFloat();
+
+        // perform the operation with the input numbers
+        float result = num1 + num2;
+
+        // display the result of the operation
+        System.out.println(num1 + " + " + num2 + " = " + result);
+
+    }
+}
