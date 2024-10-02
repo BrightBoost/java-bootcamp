182eb7045bb63d3e45daf7d4f74ffbcacf8c8af8
maaike <maaike@maaike.nl>
2024-10-02 20:48:25 +0200

    name parser

--- src/main/java/com/pluralsight/week2/FullNameParser.java
+++ src/main/java/com/pluralsight/week2/FullNameParser.java
@@ -1,4 +1,37 @@
 package com.pluralsight.week2;
 
+import java.util.Scanner;
+
 public class FullNameParser {
+
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        // get fullname input
+        System.out.println("Please enter your full name:");
+        String fullname = scanner.nextLine();
+
+        // split fullname input on the spaces and story in array string
+        String[] nameParts = fullname.split("\\s");
+
+        // create variables for nameparts
+        String firstName = "";
+        String middleName = "(none)";
+        String lastName = "";
+
+        // check length
+        if(nameParts.length == 2) {        // if length is 2: fn arr[0], mn (none), ln arr[1]
+            firstName = nameParts[0];
+            lastName = nameParts[1];
+        } else if(nameParts.length == 3) {         // if length is 3: fn arr[0], mn arr[1], ln arr[2]
+            firstName = nameParts[0];
+            middleName = nameParts[1];
+            lastName = nameParts[2];
+        } else {
+            System.out.println("ISSUE!");
+        }
+
+        System.out.println("First name: " + firstName);
+        System.out.println("Middle name: " + middleName);
+        System.out.println("Last name: " + lastName);
+    }
 }
---
+++ src/main/java/com/pluralsight/week2/Letters.java
@@ -0,0 +1,10 @@
+package com.pluralsight.week2;
+
+public class Letters {
+    public static void main(String[] args) {
+        String test = "atest";
+        char c = test.charAt(0);
+        int x = c;
+        System.out.println(x-96);
+    }
+}
